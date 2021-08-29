39b937b0c7bbe49241a56093b07376e7dab8fe6e
Maria Noriega <maria.noriega@utec.edu.pe>
2021-08-29 16:11:26 -0500

    SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { Provider as URQLProvider } from "urql";
 
 import { customTheme } from "theming";
 import { URQLClient } from "lib/client";
+import { DefaultSeo } from "next-seo";
 
 import Fonts from "theming/fonts";
 
@@ -14,6 +15,27 @@ const Minsky = ({ Component, pageProps }: AppProps) => {
     <URQLProvider value={client}>
       <ChakraProvider resetCSS theme={customTheme}>
         <Fonts />
+        <DefaultSeo
+          openGraph={{
+            type: "website",
+            locale: "es_PE",
+            url: "https://minsky.cc/",
+            site_name: "Minsky Lab",
+            images: [
+                {
+                    url: "https://i.ibb.co/dcv40rX/minsky-square-1.png",
+                    alt: "Minsky",
+                    width: 512,
+                    height: 512,
+                },
+            ],
+          }}
+          twitter={{
+            handle: "@MinskyLab",
+            site: "@MinskyLab",
+            cardType: "summary_large_image",
+          }}
+        />
         <Component {...pageProps} />
       </ChakraProvider>
     </URQLProvider>
