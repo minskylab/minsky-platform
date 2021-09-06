a091e586349d21fba98f752b3574fd2868de12c3
MistyBlunch <gracenikole@gmail.com>
2021-09-05 22:47:12 -0500

    feat: youtube video - background

--- src/modules/home/HomePageContent.tsx
+++ src/modules/home/HomePageContent.tsx
@@ -1,47 +1,69 @@
-import { Box, Container, Flex, useColorMode } from "@chakra-ui/react";
+import { Box, Container, Flex, useColorMode, chakra } from "@chakra-ui/react";
 import { Header } from "components/Header";
 import { Content } from "modules/Content";
 import { Menu } from "components/Menu";
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import Head from "next/head";
+import YouTube from "react-youtube";
 
-const bgImage = (i: number, colorMode: "light" | "dark"): string => {
-  return colorMode === "light"
-    ? "url('images/backgrounds/ac_w_" + i + ".jpg')"
-    : "url('images/backgrounds/ac_b_" + i + ".png')";
-};
+// const bgImage = (i: number, colorMode: "light" | "dark"): string => {
+//   return colorMode === "light"
+//     ? "url('images/backgrounds/ac_w_" + i + ".jpg')"
+//     : "url('images/backgrounds/ac_b_" + i + ".png')";
+// };
+
+const VideoBg = chakra(YouTube);
 
 const HomePageContent = () => {
   const { colorMode, toggleColorMode } = useColorMode();
-  const [randomIndex, setRandomIndex] = useState(1);
+  // const [randomIndex, setRandomIndex] = useState(1);
 
   const menu = [
     ["Nosotros", "Filosofía", "Tecnología"],
     ["Servicios", "Equipo", "Contactos"],
   ];
 
-  useEffect(() => {
-    setRandomIndex(Math.floor(Math.random() * 2) + 1);
-  }, []);
+  const videoOptions = {
+    playerVars: {
+      autoplay: 1,
+      color: "white",
+      controls: 0,
+      disablekb: 1,
+      loop: 1,
+      modestbranding: 1,
+      listType: "playlist",
+      list: "PLBM4KB51JM82rDde0AqEs5OufSo8sZ0dr",
+    },
+  };
+
+  // useEffect(() => {
+  //   setRandomIndex(Math.floor(Math.random() * 2) + 1);
+  // }, []);
 
   return (
-    <Box bgImage={bgImage(randomIndex, colorMode)} bgColor="rgba(0,0,0,0.7)" bgBlendMode="screen">
-      <Head>
+    <Box h="100vh">
+      <VideoBg videoId="poJJwoLIMss" opts={videoOptions} h="100vh" w="100%" pos="absolute" />
+      <Box
+        pos="relative"
+        bgColor={colorMode === "light" ? "rgba(255, 255, 255, 0.7)" : "rgba(0, 0, 0, 0.8)"}
+      >
+        <Head>
           <title>Minsky | Home</title>
           <meta name="theme-color" content="#ffdf53" />
-      </Head>
-      <Container maxW="container.xl" h="100vh" bg="transparent" padding="4" pos="relative">
-        <Header />
-        <Flex
-          align="center"
-          mx="auto"
-          height="calc(100% - 100px)"
-          w={["100%", "100%", "80%", "75%"]}
-        >
-          <Content />
-        </Flex>
-        <Menu options={menu} />
-      </Container>
+        </Head>
+        <Container maxW="container.xl" h="100vh" bg="transparent" padding="4" pos="relative">
+          <Header />
+          <Flex
+            align="center"
+            mx="auto"
+            height="calc(100% - 100px)"
+            w={["100%", "100%", "80%", "75%"]}
+          >
+            <Content />
+          </Flex>
+          <Menu options={menu} />
+        </Container>
+      </Box>
     </Box>
   );
 };
